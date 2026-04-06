f4b1604c494dc6acdd82e3c2fbdc4f9567cdf159
Edgar Poniente <edgar.poniente@gmail.com>
2026-04-07 07:48:54 +0800

    add https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -27,6 +28,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
 
         //Gate::policy(PurchaseOrder::class, PurchaseOrderPolicy::class);
     }
