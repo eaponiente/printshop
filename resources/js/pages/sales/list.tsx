309ad64cc7240d7be3879f374a82cbc6ec81b943
Edgar Poniente <edgar.poniente@gmail.com>
2026-05-09 07:31:00 +0800

    fix: only show print button on sales payments tab

--- resources/js/pages/sales/list.tsx
+++ resources/js/pages/sales/list.tsx
@@ -569,16 +569,18 @@ export default function SaleIndex({
                             <AlertCircle className="mr-1.5 h-3.5 w-3.5" />
                             Unpaid
                         </Button>
-                        <div className="ml-auto">
-                            <Button
-                                variant="outline"
-                                size="sm"
-                                onClick={() => printAllTableData('Sales Report', route('sales.print', { ...filters, tab: 'payments' }))}
-                            >
-                                <Printer className="mr-1.5 h-3.5 w-3.5" />
-                                Print
-                            </Button>
-                        </div>
+                        {activeTab === 'payments' && (
+                            <div className="ml-auto">
+                                <Button
+                                    variant="outline"
+                                    size="sm"
+                                    onClick={() => printAllTableData('Sales Report', route('sales.print', { ...filters, tab: 'payments' }))}
+                                >
+                                    <Printer className="mr-1.5 h-3.5 w-3.5" />
+                                    Print
+                                </Button>
+                            </div>
+                        )}
                     </div>
 
                     <DataTable columns={columns} tableId="sales-table" pagination={transactions} />
